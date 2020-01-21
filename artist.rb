5f10e07f97bffc61d0a24ff8e9b298664a29decf
Deanna Kelley <deanna.georges@gmail.com>
2020-01-21 23:44:17 +0000

    Automatically backed up by Learn

--- artist.rb
+++ artist.rb
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 class Artist < ActiveRecord::Base
+=======
+class Artist < ActiveRecord::Base 
+>>>>>>> fb2a2ed06c9b527b6e7bfa29279665a9d5460cd3
 end
--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,5 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
+<<<<<<< HEAD
     def up
     end
    
@@ -13,4 +14,20 @@ class CreateArtists < ActiveRecord::Migration[5.2]
           t.string :hometown
         end
     end
+=======
+  def up
+  end
+ 
+  def down
+  end
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+>>>>>>> fb2a2ed06c9b527b6e7bfa29279665a9d5460cd3
 end
